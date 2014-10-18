f9af8349f68046630892bfb13903870c4b654c8c
kimyu92 <kimyu92@cs.utexas.edu>
2014-10-18 17:24:56 -0500

    create skeleton code close #1

---
+++ src/main/java/assign/controllers/PrimaryController.java
@@ -0,0 +1,15 @@
+package assign.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PrimaryController {
+    
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+    public String getIndex()
+    {   
+    	return "pages/home"; 
+    }
+}
