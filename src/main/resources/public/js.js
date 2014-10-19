e66907ee59ff30dec2d06b39640a4185e1852b82
Ian Macalinao <me@ian.pw>
2014-10-19 05:05:05 -0500

    Make it work

--- src/main/resources/public/js.js
+++ src/main/resources/public/js.js
@@ -1,4 +1,5 @@
-(function() {
+$(function() {
+	var strategy = [];
 
 	var actionVerbs = {
 		buy: [
@@ -33,21 +34,61 @@
 
 	var dictionary = [actionVerbs, worthless, securities];
 
+	$('#command-input').keypress(function(e){
+	    if(e.which !== 13) {
+	    	return;
+	    }
 
-
-	$('#command-input').bind('input', function() {
-		var inp = $(this).val();
-		console.log(inp);
-	});
-
-
-	$('#command-input').bind("enterKey", function(e){
 		var inp = $(this).val();
 
 		$("#rules").append($("<li>").text(inp));
-
+		strategy.push(inp);
 
 		$(this).val('');
 	});
 
-})();
+	$('#goButton').click(function() {
+		$('#preQuery .command').addClass('animated fadeOutUpBig');
+		$('#preQuery .signals').addClass('animated fadeOutDownBig');
+		$('#loadingSpinner').removeClass('hide').addClass('animated fadeIn');
+		(new Spinner({
+			color: '#ccc',
+			radius: 60,
+			length: 40,
+			width: 20
+		})).spin(document.getElementById('loadingSpinner'));
+
+		$.post('http://localhost:4567/strategy_test', {
+			initial: 10000,
+			stock: 'ACN US Equity',
+			strategy: strategy.join('; ')
+		}, function(data) {
+			if (data === 'omg') {
+				alert('Invalid input.');
+				return;
+			}
+			$('#preQuery').addClass('animated fadeOutLeftBig');
+			$('#postQuery').removeClass('hide').addClass('animated fadeInRightBig');
+			
+			window.jdata = JSON.parse(data);
+
+			$('#btResults').highcharts('StockChart', {
+				rangeSelector: {
+					selected: 100000
+				},
+				title: {
+					text: 'AAPL Stock Price'
+				},
+				series: [{
+					name: 'AAPL',
+					data: _.map(jdata.dailyData, function(item) {
+						return [new Date(item.date), item.value];
+					}),
+					tooltip: {
+						valueDecimals: 2
+					}
+				}]
+			})
+		});
+	});
+});
